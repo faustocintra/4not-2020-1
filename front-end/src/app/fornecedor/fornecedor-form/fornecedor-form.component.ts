ab45a0e1cd21eefc5802d0e78e0bb9969653d8ce
Fausto G. Cintra <fausto@fgcintra.pro.br>
2020-06-17 22:27:39 -0300

    (17/06) Implementaçao da logica de formularios

--- front-end/src/app/fornecedor/fornecedor-form/fornecedor-form.component.ts
+++ front-end/src/app/fornecedor/fornecedor-form/fornecedor-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FornecedorService } from '../fornecedor.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-fornecedor-form',
@@ -11,9 +15,59 @@ export class FornecedorFormComponent implements OnInit {
 
   fornecedor : any = {}   // Objeto vazio
 
-  constructor() { }
+  constructor(
+    private fornecedorSrv : FornecedorService,
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private actRoute: ActivatedRoute
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    // Capturando os parâmetros da rota
+    let params = this.actRoute.snapshot.params
+
+    // Existe um parâmetro chamado :id?
+    if(params['id']) {
+      // É caso de atualização. É necesário consultar o back-end
+      // para recuperar o registro e colocá-lo para edição
+      try {
+        this.fornecedor = await this.fornecedorSrv.obterUm(params['id'])
+        this.title = 'Atualizando fornecedor'
+      }
+      catch(erro) {
+        this.snackBar.open(erro.message, 'Que pena!', {duration: 5000})
+      }
+    }
+  
+  }
+
+  voltar(x) {
+
+  }
+
+  async salvar(form: NgForm) {
+    // Só tenta salvar se o form for válido
+    if(form.valid) {
+      try {
+        let msg = 'Fornecedor atualizado com sucesso.'
+        // Se existir o campo _id, é caso de atualização
+        if(this.fornecedor._id) {
+          await this.fornecedorSrv.atualizar(this.fornecedor)
+        }
+        // Senão, é caso de criar um novo fornecedor
+        else {
+          await this.fornecedorSrv.novo(this.fornecedor)
+          msg = 'Fornecedor criado com sucesso.'
+        }
+        // Dá o feedback para o usuário
+        this.snackBar.open(msg, 'Entendi', {duration: 5000})
+        // Voltar à listagem
+        this.router.navigate(['/fornecedor'])
+      }
+      catch(erro) {
+        this.snackBar.open(erro.message, 'Que pena!', {duration: 5000})
+      }
+    }
   }
 
 }
