e5787f938e512c2a2d223fa87daeff7676695eb3
Fausto G. Cintra <fausto@fgcintra.pro.br>
2020-07-01 21:29:54 -0300

    (01/07) Finalizaçao da logica mestre/detalhe para venda/item-venda

--- front-end/src/app/item-venda/item-venda-form/item-venda-form.component.ts
+++ front-end/src/app/item-venda/item-venda-form/item-venda-form.component.ts
@@ -6,6 +6,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Location } from '@angular/common';
+import { ConfirmDlgComponent } from 'src/app/ui/confirm-dlg/confirm-dlg.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-item-venda-form',
@@ -15,6 +17,8 @@ import { Location } from '@angular/common';
 export class ItemVendaFormComponent implements OnInit {
 
   title: string = 'Novo item de venda'
+
+  vendaDisabled : boolean = false
 
   itemVenda : any = { desconto: 0, acrescimo: 0 }  
 
@@ -29,7 +33,8 @@ export class ItemVendaFormComponent implements OnInit {
     private snackBar: MatSnackBar,
     private router: Router,
     private actRoute: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private dialog: MatDialog
   ) { }
 
   async ngOnInit() {
@@ -43,10 +48,23 @@ export class ItemVendaFormComponent implements OnInit {
       try {
         this.itemVenda = await this.itemVendaSrv.obterUm(params['id'])
         this.title = 'Atualizando item de venda'
+
+        // Impede a alteração da venda relacionada ao item
+        this.vendaDisabled = true
+
       }
       catch(erro) {
         this.snackBar.open(erro.message, 'Que pena!', {duration: 5000})
       }
+    }
+
+    // Existe um parâmetro chamado :venda?
+    if(params['venda']) {
+      // Forçar o id da venda no item de venda
+      this.itemVenda.venda = params['venda']
+      
+      // Desabilita o select da venda
+      this.vendaDisabled = true
     }
 
     // Entidades relacionadas
@@ -60,7 +78,25 @@ export class ItemVendaFormComponent implements OnInit {
   
   }
 
-  voltar(x) {
+  async voltar(form: NgForm) {
+    
+    let result = true;
+    console.log(form);
+    // form.dirty = formulário "sujo", não salvo (via código)
+    // form.touched = o conteúdo de algum campo foi alterado (via usuário)
+    if(form.dirty && form.touched) {
+      let dialogRef = this.dialog.open(ConfirmDlgComponent, {
+        width: '50%',
+        data: { question: 'Há dados não salvos. Deseja realmente voltar?' }
+      });
+
+      result = await dialogRef.afterClosed().toPromise();
+
+    }
+
+    if(result) {
+      this.location.back() // Retorna à listagem
+    }
 
   }
 
--- front-end/src/app/venda/venda-form/venda-form.component.ts
+++ front-end/src/app/venda/venda-form/venda-form.component.ts
@@ -1,9 +1,11 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { VendaService } from '../venda.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { ClienteService } from 'src/app/cliente/cliente.service';
+import { ConfirmDlgComponent } from 'src/app/ui/confirm-dlg/confirm-dlg.component';
 
 @Component({
   selector: 'app-venda-form',
@@ -43,7 +45,8 @@ export class VendaFormComponent implements OnInit {
     private clienteSrv : ClienteService,
     private snackBar: MatSnackBar,
     private router: Router,
-    private actRoute: ActivatedRoute
+    private actRoute: ActivatedRoute,
+    private dialog: MatDialog
   ) { }
 
   async ngOnInit() {
@@ -73,7 +76,25 @@ export class VendaFormComponent implements OnInit {
   
   }
 
-  voltar(x) {
+  async voltar(form: NgForm) {
+    
+    let result = true;
+    console.log(form);
+    // form.dirty = formulário "sujo", não salvo (via código)
+    // form.touched = o conteúdo de algum campo foi alterado (via usuário)
+    if(form.dirty && form.touched) {
+      let dialogRef = this.dialog.open(ConfirmDlgComponent, {
+        width: '50%',
+        data: { question: 'Há dados não salvos. Deseja realmente voltar?' }
+      });
+
+      result = await dialogRef.afterClosed().toPromise();
+
+    }
+
+    if(result) {
+      this.router.navigate(['/venda']); // Retorna à listagem
+    }
 
   }
 
